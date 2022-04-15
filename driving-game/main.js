269c5089592074b1d059727e89ed3434704e6673
Dylan De Vei <dylan.devei@gmail.com>
2022-04-15 21:35:34 +0000

    completed issue-2-user-can-turn-their-car #2.

--- driving-game/main.js
+++ driving-game/main.js
@@ -1,1 +1,15 @@
 var $car = document.querySelector('.car');
+
+window.addEventListener('keydown', rotateCar);
+
+function rotateCar(event) {
+  if (event.key === 'ArrowDown') {
+    $car.className = ('class', 'car down');
+  } else if (event.key === 'ArrowUp') {
+    $car.className = ('class', 'car up');
+  } else if (event.key === 'ArrowLeft') {
+    $car.className = ('class', 'car left');
+  } else if (event.key === 'ArrowRight') {
+    $car.className = ('class', 'car right');
+  }
+}
